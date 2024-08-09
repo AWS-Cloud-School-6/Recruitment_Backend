54a63f29f8cf37c0b8a15f9074315793c473a529
gytjd <hys339631@gmail.com>
2024-08-09 23:59:31 +0900

    #24 [Feat]: Response Entity 생성

---
+++ src/main/java/Aws6/Recruitment/entity/response/CommonResult.java
@@ -0,0 +1,11 @@
+package Aws6.Recruitment.entity.response;
+
+import lombok.Data;
+
+@Data
+public class CommonResult { // api 실행 결과를 담는 공통 모델
+    private boolean success;
+    private int code;
+    private String msg;
+
+}
