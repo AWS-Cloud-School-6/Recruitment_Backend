ec231aca2db7cef8b107ae6e9df852151f333893
gytjd <hys339631@gmail.com>
2024-08-14 00:53:45 +0900

    #40 [Feat]: RabbitMQ 설정

---
+++ src/main/java/Aws6/Recruitment/api/dto/message/MessageDto.java
@@ -0,0 +1,14 @@
+package Aws6.Recruitment.api.dto.message;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+
+    private String title;
+    private String content;
+}
